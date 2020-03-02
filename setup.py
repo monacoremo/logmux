89f19e1b133c1a0832647ba11040869b29d942d5
Remo <59358383+monacoremo@users.noreply.github.com>
2020-03-02 23:03:28 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='logmux',
+    version='0.1.0',
+    description='Tail multiple log files, label their lines and combine them in one stream',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/monacoremo/logmux',
+    author='monacoremo',
+    author_email='monacohacks@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='logs tail monitoring',
+    py_modules=["logmux"],
+    python_requires='>=3.8, <4',
+    install_requires=['click'],
+    extras_require={
+        'test': ['pytest'],
+    },
+    entry_points={
+        'console_scripts': [
+            'logmux=logmux:main',
+        ],
+    },
+)
