c7b10852908de2f13ca0fde4f83253e7cebbc36a
Remo <59358383+monacoremo@users.noreply.github.com>
2020-03-02 23:29:22 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,8 +1,16 @@
 import pytest
 import subprocess
+import tempfile
+from multiprocessing import Process
 
 import logmux
 
 
 def test_basics():
-    pass
+    with tempfile.NamedTemporaryFile() as log1,
+         tempfile.NamedTemporaryFile() as log2,
+         tempfile.NamedTemporaryFile() as log3:
+
+        paths = [log1.name, log2.name, log3.name]
+        
+        process = Process(logmux.main, paths)
